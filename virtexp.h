f5ea60f4a95b6fada58dfcec640929b4949bf06e
David Shin <dk.shin1992@gmail.com>
2015-07-14 17:06:43 +1200

    Finish scientific notation reporting

--- virtexp.h
+++ virtexp.h
@@ -101,7 +101,8 @@ class VariablesHolder
 		{
 			for(ALLELE::iterator it=m_Vars.begin();it!=m_Vars.end();++it)
 			{
-				fprintf(pfout,"%s=%lf  ",convert(it->first).c_str(),it->second);
+				fprintf(pfout,"%s=%.5e  ",convert(it->first).c_str(),it->second);
+				//fprintf(pfout,"%s=%lf  ",convert(it->first).c_str(),it->second);
 			}
 			fprintf(pfout,"\n");
 		}
