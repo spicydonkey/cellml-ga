0999677a54923b12d90a58c1bc49716245e9a1f0
David Shin <dk.shin1992@gmail.com>
2015-07-16 11:27:56 +1200

    Tidy main source includes

--- experiment.cpp
+++ experiment.cpp
@@ -1,9 +1,5 @@
 #include <mpi.h>
-//#include "cellml-api-cxx-support.hpp"
-//#include "IfaceCellML_APISPEC.hxx"
-//#include "IfaceCIS.hxx"
-//#include "CellMLBootstrap.hpp"
-//#include "CISBootstrap.hpp"
+#include "cellml_observer.h"
 #include "distributor.h"
 #include <unistd.h>
 #include <stdlib.h>
